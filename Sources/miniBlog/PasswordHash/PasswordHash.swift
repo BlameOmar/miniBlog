0bf7196db70b4996e29554e6b488736ff8a3e2c5
Omar Evans <omar@omarevans.com>
2022-09-25 23:26:04 -0400

    Remove public visibility

--- Sources/miniBlog/PasswordHash/PasswordHash.swift
+++ Sources/miniBlog/PasswordHash/PasswordHash.swift
@@ -1,4 +1,4 @@
-public struct PasswordHash {
+struct PasswordHash {
     enum HashAlgorithm: String, CaseIterable {
         case argon2i = "$argon2i$"
         case argon2d = "$argon2d$"
@@ -16,40 +16,40 @@ extension PasswordHash.HashAlgorithm {
 }
 
 extension PasswordHash: CustomStringConvertible {
-    public var description: String {
+    var description: String {
         hash.description
     }
 }
 
 extension PasswordHash: CustomDebugStringConvertible {
-    public var debugDescription: String {
+    var debugDescription: String {
         #"PasswordHash(algorithm: .\#(algorithm), hash: "\#(self.hash)")"#
     }
 }
 
 extension PasswordHash: Encodable {
-    public func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws {
         try hash.encode(to: encoder)
     }
 }
 
 extension PasswordHash: Decodable {
-    public enum DecodeError: Error {
+    enum DecodeError: Error {
         case unsupportedHashAlgorithm
     }
 
-    public init(rawHash: String) throws {
+    init(rawHash: String) throws {
         guard let detected = HashAlgorithm.allCases.first(where: { rawHash.starts(with: $0.hashPrefix) }) else {
             throw DecodeError.unsupportedHashAlgorithm
         }
         self.init(algorithm: detected, hash: rawHash)
     }
 
-    public init?(_ hash: String) {
+    init?(_ hash: String) {
         try? self.init(rawHash: hash)
     }
 
-    public init(from decoder: Decoder) throws {
+    init(from decoder: Decoder) throws {
         let rawHash = try String(from: decoder)
         try self.init(rawHash: rawHash)
     }
